da79932fe1b3e7a19bfec3a534de7dc0bf2f163b
Zeroization <1255108389@qq.com>
2025-10-07 00:35:32 +0800

    完成 S2L10
    - 实现 AuraPlayerController 相关内容（设置IMC，鼠标指针）

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -3,6 +3,35 @@
 
 #include "Player/AuraPlayerController.h"
 
+#include "EnhancedInputSubsystems.h"
 
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
 
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
 
+	// 设置InputMappingContext
+	checkf(IMC_AuraContext, TEXT("IMC_AuraContext is not set !"));
+	if (auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		Subsystem->AddMappingContext(IMC_AuraContext, 0);
+	}
+	else
+	{
+		checkf(false, TEXT("Can't get UEnhancedInputLocalPlayerSubsystem !"));
+	}
+
+	// 设置鼠标指针相关
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Type::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+	
+}
