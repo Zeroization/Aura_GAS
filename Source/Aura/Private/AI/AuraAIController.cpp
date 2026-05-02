f20ac59d0505e16eb05cb5538753b1e65bf6fafd
Zeroization <1255108389@qq.com>
2026-05-02 17:09:07 +0800

    完成 S15 L164
    新特性：
    - 初始化敌人AI相关组件（AIController，黑板和行为树）

---
+++ Source/Aura/Private/AI/AuraAIController.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AI/AuraAIController.h"
+
+#include "BehaviorTree/BehaviorTreeComponent.h"
+#include "BehaviorTree/BlackboardComponent.h"
+
+
+AAuraAIController::AAuraAIController()
+{
+    Blackboard = CreateDefaultSubobject<UBlackboardComponent>("Blackboard");
+    BehaviorTreeComponent = CreateDefaultSubobject<UBehaviorTreeComponent>("BehaviorTree");
+}
+
+void AAuraAIController::BeginPlay()
+{
+    Super::BeginPlay();
+}
